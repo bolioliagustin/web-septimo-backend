087ca92ded337603253c2dd2d7e47f488a8f4fcb
BolioliAgustin <bolioliagustin@gmail.com>
2022-07-20 15:49:48 -0300

    s

--- src/api/noticia/services/noticia.js
+++ src/api/noticia/services/noticia.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service.
+ * noticia service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::noticia.noticia');
