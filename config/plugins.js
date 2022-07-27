9c72b86b59363c7c4448334e78ebf8326bc69c4e
BolioliAgustin <bolioliagustin@gmail.com>
2022-07-27 16:42:58 -0300

    s

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,24 @@
-module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
-      },
-    },
-  });
+module.exports = ({ env }) => {
+    if (env('NODE_ENV') === 'production') {
+        return {
+            upload: {
+                config: {
+                    provider: 'cloudinary',
+                    providerOptions: {
+                        cloud_name: env('CLOUDINARY_NAME'),
+                        api_key: env('CLOUDINARY_KEY'),
+                        api_secret: env('CLOUDINARY_SECRET'),
+                    },
+                    actionOptions: {
+                        upload: {},
+                        uploadStream: {},
+                        delete: {},
+                    },
+                },
+            },
+        }
+    }
+    return {
+
+    }
+};
